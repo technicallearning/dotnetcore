f50ec00b8b7a9c141dd51e2a284edd6ad5f8c2d0
Phi Nguyen <nnphi219@gmail.com>
2018-05-31 23:30:49 +0700

    working

---
+++ JobPortalRestore.DAL/data/JobPortalRestoreWebContext.cs
@@ -0,0 +1,16 @@
+﻿using JobPortalRestore.DAL.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobPortalRestore.DAL.data
+{
+    public class JobPortalRestoreWebContext: DbContext
+    {
+        public JobPortalRestoreWebContext(DbContextOptions<JobPortalRestoreWebContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ JobPortalRestore/Controllers/MoviesController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JobPortalRestore.DAL.data;
+using JobPortalRestore.DAL.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobPortalRestore.Web.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly JobPortalRestoreWebContext _context;
+
+        public MoviesController(JobPortalRestoreWebContext context)
+        {
+            _context = context;
+        }
+        // GET: Movie
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Movie.ToListAsync());
+        }
+
+        // GET: Movie/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                    .SingleOrDefaultAsync(m => m.ID == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movie/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movie/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID, Title, ReleaseDate, Genre, Price")] Movie movie)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movie/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.SingleOrDefaultAsync(m => m.ID == id);
+            if(movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movie/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID, Title, ReleaseDate, Genre, Price")] Movie movie)
+        {
+            if(id != movie.ID)
+            {
+                return NotFound();
+            }
+        }
+
+        // GET: Movie/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
